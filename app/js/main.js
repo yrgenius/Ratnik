98a2d7a1e04d78767acbbc092fd0c0e81dc9d967
Oblivion <yrgenius80@gmail.com>
2022-04-21 19:31:41 +0900

    last version

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,9 @@ let textBlock = document.getElementsByClassName('main__content');
 
 let mainTitle = document.getElementsByClassName('main__title');
 
-for(let i = 0; i < mainTitle.length; i++){
-    mainTitle[i].onclick = () =>{
+for (let i = 0; i < mainTitle.length; i++) {
+    mainTitle[i].onclick = () => {
         console.log(mainTitle[i].classList.toggle('arrow-rotate'));
         textBlock[i].classList.toggle('active');
-    }; 
+    };
 }
-
-
-
-
-
-
-
-
